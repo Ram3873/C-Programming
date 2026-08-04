779075c45fae237f39239fc80a158c2d89561436
Ram Sharma <sharmaram6385@gmail.com>
2026-08-04 02:08:20 -0400

    Add files via upload

---
+++ result.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int marks = 50;
+    if (marks >= 100)
+    {
+        printf("pass");
+    }
+    else
+    {
+        printf("fail");
+    }
+    return 0;
+}
